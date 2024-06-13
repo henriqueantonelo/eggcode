7ff2b0195db9a576a3042476b629afebd7d7a30b
Henrique Antonelo <henrique.antonelo@gmail.com>
2024-06-13 16:16:36 -0300

    implemented theme functionality

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-// Challenge nº1
+///Funcionamento do switch de temas
+document.addEventListener("DOMContentLoaded", function () {
+  let theme_toggler = document.getElementById("theme-btn");
+
+  // Função para alternar o tema
+  function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+
+    // Verificar se a classe 'dark-mode' está presente no corpo do documento
+    let isDarkMode = document.body.classList.contains("dark-mode");
+
+    // Armazenar a preferência de tema no localStorage
+    localStorage.setItem("tema", isDarkMode ? "dark" : "light");
+  }
+
+  theme_toggler.addEventListener("click", toggleTheme);
+
+  let temaArmazenado = localStorage.getItem("tema");
+
+  if (temaArmazenado === "dark") {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+});
+
 const calcAge = function (age) {
   age = age * 365;
   calcAgeInput = document.getElementById("calc-age");
